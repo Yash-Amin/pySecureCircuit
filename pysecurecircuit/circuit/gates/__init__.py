85bd8d90a1e75bd69d2c43b7997a911ed615ad24
Yash Amin <aminyash8@gmail.com>
2023-04-12 10:07:12 -0500

    Add circuit gates

---
+++ pysecurecircuit/circuit/gates/__init__.py
@@ -0,0 +1,5 @@
+from pysecurecircuit.circuit.gates.gate import Gate
+from pysecurecircuit.circuit.gates._and import AndGate
+from pysecurecircuit.circuit.gates._or import OrGate
+from pysecurecircuit.circuit.gates._xor import XorGate
+from pysecurecircuit.circuit.gates._xnor import XnorGate
